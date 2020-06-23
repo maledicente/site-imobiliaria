9412b3f3fa2696ede4699b4cb4cb9d33ac49b756
maledicente <luizpaulonievola@ymail.com>
2020-06-23 19:43:27 -0300

    Arquivos iniciais

---
+++ header.php
@@ -0,0 +1,29 @@
+<!-- Start Header -->
+<header>
+	<div class="conteiner-top">
+		<a href="#"><img src="imagens/logo.svg" alt="Casa"></a>
+		<div class="menu-nav">
+			<nav>
+				<ul class="menu">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Imoveis</a>
+					<ul>
+						<li><a href="#">Apartamentos</a></li>
+						<li><a href="#">Casas</a></li>
+						<li><a href="#">Sobrados</a></li>
+						<li><a href="#">Outros</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Contato</a></li>
+				<li><a href="#">Login</a></li>
+			</ul>
+		</nav>
+	</div>
+	<a href="#"><img src="imagens/menu.svg" alt="Menu"></a>
+</div>
+<form class="form-busca" action="busca.html" method="get">
+	<input class="busca" type="text" placeholder="Procurar...">
+	<button class="btn">Buscar</button>
+</form>
+</header>
+<!-- End Header -->
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<title>Imobiliaria Nievola</title>
+		<link rel="stylesheet" href="css/style.css">
+		<script src="js/scripts.js"></script>
+	</head>
+	<body>
+		<?php
+			include 'header.php';
+		?>
+		
+	</body>
+</html>
